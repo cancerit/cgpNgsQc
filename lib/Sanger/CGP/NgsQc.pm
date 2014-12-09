dc06a4f4f005362afd3102b8a4b10621c5464d9a
Keiran Raine <keiranmraine@gmail.com>
2014-12-09 14:46:16 +0000

    First cleaned up version

--- lib/Sanger/CGP/NgsQc.pm
+++ lib/Sanger/CGP/NgsQc.pm
@@ -2,8 +2,23 @@ package Sanger::CGP::NgsQc;
 
 use strict;
 use base 'Exporter';
+use Bio::DB::Sam;
 
 our $VERSION = '0.0.1';
 our @EXPORT = qw($VERSION);
+
+sub bam_sample_name {
+  my $bam = shift;
+  my @lines = split /\n/, Bio::DB::Sam->new(-bam => $bam)->header->text;
+  my $sample;
+  for(@lines) {
+    if($_ =~ m/^\@RG.*\tSM:([^\t]+)/) {
+      $sample = $1;
+      last;
+    }
+  }
+  die "Failed to determine sample from BAM header\n" unless(defined $sample);
+  return $sample;
+}
 
 1;
