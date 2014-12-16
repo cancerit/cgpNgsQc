7234d3b759183217b589b38b07be4763fed4fa59
Keiran Raine <keiranmraine@gmail.com>
2014-12-16 09:19:34 +0000

    Corrects issue with getting path of verifyBamId and bump version to 1.0.0

--- lib/Sanger/CGP/NgsQc.pm
+++ lib/Sanger/CGP/NgsQc.pm
@@ -2,24 +2,24 @@ package Sanger::CGP::NgsQc;
 
 ########## LICENCE ##########
 # Copyright (c) 2014 Genome Research Ltd.
-# 
+#
 # Author: Keiran Raine <cgpit@sanger.ac.uk>
-# 
+#
 # This file is part of cgpNgsQc.
-# 
+#
 # cgpNgsQc is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Affero General Public License as published by the Free
 # Software Foundation; either version 3 of the License, or (at your option) any
 # later version.
-# 
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
 # details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 # 1. The usage of a range of years within a copyright statement contained within
 # this distribution should be interpreted as being equivalent to a list of years
 # including the first and last year specified and all consecutive years between
@@ -36,7 +36,7 @@ use strict;
 use base 'Exporter';
 use Bio::DB::Sam;
 
-our $VERSION = '0.0.1';
+our $VERSION = '1.0.0';
 our @EXPORT = qw($VERSION);
 
 sub bam_sample_name {
