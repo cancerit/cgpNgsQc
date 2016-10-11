be6352ced0fd5cfedf4f238d3ef9f3a864c4f8cd
Keiran Raine <keiranmraine@gmail.com>
2016-10-11 19:15:50 +0100

    improve setup and add travis

--- setup.sh
+++ setup.sh
@@ -41,25 +41,20 @@ get_file () {
   fi
 }
 
-done_message () {
-    if [ $? -eq 0 ]; then
-        echo " done."
-        if [ "x$1" != "x" ]; then
-            echo $1
-        fi
-    else
-        echo " failed.  See setup.log file for error messages." $2
-        echo "    Please check INSTALL file for items that should be installed by a package manager"
-        exit 1
-    fi
-}
-
-if [ "$#" -ne "1" ] ; then
-  echo "Please provide an installation path  such as /opt/pancan"
+if [[ ($# -ne 1 && $# -ne 2) ]] ; then
+  echo "Please provide an installation path and optionally perl lib paths to allow, e.g."
+  echo "  ./setup.sh /opt/myBundle"
+  echo "OR all elements versioned:"
+  echo "  ./setup.sh /opt/cgpVcf-X.X.X /opt/PCAP-X.X.X/lib/perl"
   exit 0
 fi
 
 INST_PATH=$1
+
+if [[ $# -eq 2 ]] ; then
+  CGP_PERLLIBS=$2
+fi
+
 
 CPU=`grep -c ^processor /proc/cpuinfo`
 if [ $? -eq 0 ]; then
@@ -74,21 +69,7 @@ echo "Max compilation CPUs set to $CPU"
 # get current directory
 INIT_DIR=`pwd`
 
-# re-initialise log file
-echo > $INIT_DIR/setup.log
-
-# log information about this system
-(
-    echo '============== System information ===='
-    set -x
-    lsb_release -a
-    uname -a
-    sw_vers
-    system_profiler
-    grep MemTotal /proc/meminfo
-    set +x
-    echo; echo
-) >>$INIT_DIR/setup.log 2>&1
+set -e
 
 # cleanup inst_path
 mkdir -p $INST_PATH/bin
@@ -99,7 +80,24 @@ cd $INIT_DIR
 # make sure that build is self contained
 unset PERL5LIB
 PERLROOT=$INST_PATH/lib/perl5
-export PERL5LIB="$PERLROOT"
+
+# allows user to knowingly specify other PERL5LIB areas.
+if [ -z ${CGP_PERLLIBS+x} ]; then
+  export PERL5LIB="$PERLROOT"
+else
+  export PERL5LIB="$PERLROOT:$CGP_PERLLIBS"
+fi
+
+export PATH=$INST_PATH/bin:$PATH
+
+#create a location to build dependencies
+SETUP_DIR=$INIT_DIR/install_tmp
+mkdir -p $SETUP_DIR
+
+## grab cpanm and stick in workspace, then do a self upgrade into bin:
+get_file $SETUP_DIR/cpanm https://cpanmin.us/
+perl $SETUP_DIR/cpanm -l $INST_PATH App::cpanminus
+CPANM=`which cpanm`
 
 CHK=`perl -le 'eval "require $ARGV[0]" and print $ARGV[0]->VERSION' Bio::DB::HTS`
 if [[ "x$CHK" == "x" ]] ; then
@@ -107,7 +105,7 @@ if [[ "x$CHK" == "x" ]] ; then
   exit 1;
 fi
 
-CHK=`which $INST_PATH/bin/alleleCounter`
+CHK=`which alleleCounter`
 if [[ "x$CHK" == "x" ]] ; then
   echo "PREREQUISITE: Please install alleleCount before proceeding: https://github.com/cancerit/alleleCount/releases"
   exit 1;
@@ -117,46 +115,30 @@ perlmods=( "File::ShareDir::Install" )
 
 set -e
 for i in "${perlmods[@]}" ; do
-  echo -n "Installing build prerequisite $i..."
-  (
-    $INIT_DIR/bin/cpanm -v --mirror http://cpan.metacpan.org -l $INST_PATH $i
-  ) >>$INIT_DIR/setup.log 2>&1
-  done_message "" "Failed during installation of $i."
+  echo "Installing build prerequisite $i..."
+  $CPANM --no-interactive --notest --mirror http://cpan.metacpan.org -l $INST_PATH $i
 done
-
-#add bin path for install tests
-export PATH="$INST_PATH/bin:$PATH"
 
 cd $INIT_DIR
 
-echo -n "Installing Perl prerequisites ..."
+echo "Installing Perl prerequisites ..."
 if ! ( perl -MExtUtils::MakeMaker -e 1 >/dev/null 2>&1); then
-    echo
-    echo "WARNING: Your Perl installation does not seem to include a complete set of core modules.  Attempting to cope with this, but if installation fails please make sure that at least ExtUtils::MakeMaker is installed.  For most users, the best way to do this is to use your system's package manager: apt, yum, fink, homebrew, or similar."
+  echo "WARNING: Your Perl installation does not seem to include a complete set of core modules.  Attempting to cope with this, but if installation fails please make sure that at least ExtUtils::MakeMaker is installed.  For most users, the best way to do this is to use your system's package manager: apt, yum, fink, homebrew, or similar."
 fi
-(
-  $INIT_DIR/bin/cpanm -v --mirror http://cpan.metacpan.org --notest -l $INST_PATH/ --installdeps . < /dev/null
-) >>$INIT_DIR/setup.log 2>&1
-done_message "" "Failed during installation of core dependencies."
+$CPANM --no-interactive --notest --mirror http://cpan.metacpan.org -l $INST_PATH/ --installdeps . < /dev/null
 
-#create a location to build dependencies
-SETUP_DIR=$INIT_DIR/install_tmp
-mkdir -p $SETUP_DIR
 cd $SETUP_DIR
 get_file "verifyBamId" $SOURCE_VERIFYBAM
 cp $SETUP_DIR/verifyBamId $INST_PATH/bin/.
 chmod +x $INST_PATH/bin/verifyBamId
 cd $INIT_DIR
 
-echo -n "Installing cgpNgsQc ..."
-(
-  cd $INIT_DIR &&
-  perl Makefile.PL INSTALL_BASE=$INST_PATH &&
-  make &&
-  make test &&
-  make install
-) >>$INIT_DIR/setup.log 2>&1
-done_message "" "cgpNgsQc install failed."
+echo "Installing cgpNgsQc ..."
+cd $INIT_DIR
+perl Makefile.PL INSTALL_BASE=$INST_PATH
+make
+make test
+make install
 
 # cleanup all junk
 rm -rf $SETUP_DIR
